85036b24635d9cc9dc4c07cbc4888edf46ca3f3b
SethStephens32 <sethstephens2018@gmail.com>
2023-01-21 17:56:11 -0600

    finished product

---
+++ kanban-board/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import Kanban from './components/Kanban/Kanban';
+
+function App() {
+  return (
+    <div className="App-Container">
+      <h1 className='container-header'>Kanban React App</h1>
+      <Kanban/>
+    </div>
+  );
+}
+
+export default App;
